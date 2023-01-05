bf06feea37c8bbd794b8008a9d3499fa34e5b12f
Mats Ribaucourt <mats-ribaucourt@hotmail.com>
2023-01-05 17:34:23 +0700

    add multiple hearts and colors

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
-import HeartLogo from './heart.svg';
+import HeartsList from './components/HeartsList';
+import Header from './components/Header';
 import './App.css';
-
-const message = "cool cud";
-const headerMessage = "I ❤️ React";
-
-const Heart = (props) => {
-  return (
-    <div className="heart">
-      <img className="heart-img" src={HeartLogo} alt="heart" />
-      <p className="heart-message">{props.msg}</p>
-    </div>
-  )
-}
-
-const Header = (props) => { 
-  return <div className="App-header">{props.msg}</div>
-}
 
 function App() {
   return (
     <>
-      <Header msg={headerMessage}/>
-      <Heart msg={message} />
+      <Header />
+      <HeartsList />
     </>
   );
 }
